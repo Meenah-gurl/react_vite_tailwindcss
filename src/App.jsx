dd057c1e14e98ddd4643e66c46abd9b36b1fe186
meenah-gurl <aminasani.as64@gmail.com>
2022-10-31 05:09:18 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const App = () => {
+  return (
+   <div className='py-10'>
+      <div className=' text-white font-serif flex justify-center items-center py-2 w-auto'>
+          <h2 className='w-auto bg-blue-600 rounded-lg px-3 py-2'>Congratulation you finally Install React + Vite + TailwindCss</h2>
+      </div>
+   </div>
+
+  )
+}
+
+export default App
